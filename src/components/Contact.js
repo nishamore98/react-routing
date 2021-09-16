6d056df5a4927807cee2709b18e8ee358cd50b84
nishamore98 <nisha.more9498@gmail.com>
2021-09-16 13:12:32 +0530

    Api integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,28 @@ import React from 'react';
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <h3>Contact</h3>
+      <div className="contact-form">
+        <input
+          placeholder="Username"
+          type="text"
+          name="username"
+          className="my-form"
+        />
+        <br />
+        <br />
+        <input
+          placeholder="Password"
+          type="password"
+          name="password"
+          className="my-form"
+        />
+        <br />
+        <br />
+        <button className="submit-btn my-form" onclick="myfun">
+          Submit
+        </button>
+      </div>
     </>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [userdata, setuserdata] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://reqres.in/api/users?page=2')
+      .then(res => setuserdata(res.data.data));
+  }, []);
+
   return (
     <>
-      <h1>Home</h1>
+      <h3>Home</h3>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>e-mail</th>
+        </tr>
+        {userdata?.map((item, index) => {
+          return (
+            <tr className="home-table">
+              <td>{item.id}</td>
+              <td>
+                {item.first_name}
+                {item.last_name}
+              </td>
+              <td>{item.email}</td>
+            </tr>
+          );
+        })}
+      </table>
     </>
   );
 };
